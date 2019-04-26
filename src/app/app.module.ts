c9f568781df6a6d340ec7170dfa3ac7f649614c9
Blaze Fugina <blaze.fugina@gmail.com>
2019-04-25 23:37:36 -0500

    Committing this example.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HomeComponent } from './home/home/home.component';
+import { MatTabsModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
+import { HeaderComponent } from './navigation/navigation/header/header.component';
+import { SidenavListComponent } from './navigation/navigation/sidenav-list/sidenav-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
